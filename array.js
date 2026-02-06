d88ea2d1135a6e04b479a595140a969a5a91b5b5
fatematujjohra <fatematujjohralima2@gmail.com>
2026-02-06 13:47:34 +0600

    indexOf is array join concat slice

--- array.js
+++ array.js
@@ -1,3 +1,15 @@
+/* array methods
+1.includes()
+2.indexOf()
+3.isArray()
+4.join()-----returns array as a string
+5.concat()
+6.slice()
+7.splice()
+*/
+
+
+
 const numbers = [1,2,3,4,5,6,7];
 //  element push in last position
 numbers.push(8);
@@ -20,3 +32,15 @@ else{
     numbers.pop();
 }
 console.log(numbers);
+// check array
+const food = ['apple','banana','watermelon','strawberry'];
+const cat='meow'
+console.log(Array.isArray(food));
+console.log(Array.isArray(cat));
+
+// indexOf()---->return index if element exist
+console.log(food.indexOf('banana'));
+// returns array as a string
+console.log(numbers.join('+'));
+console.log(numbers.concat(food));
+console.log(food.slice(1,3));
