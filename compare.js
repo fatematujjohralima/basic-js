3971c009f6bdb65c5785d111a7c210309ed1220a
fatematujjohra <fatematujjohralima2@gmail.com>
2026-02-07 09:50:22 +0600

    toLowerCase()

---
+++ compare.js
@@ -0,0 +1,9 @@
+const subject = 'Chemistry';
+const book = 'ChemIsTry';
+
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log('its chemistry');
+}
+else {
+    console.log('other subject');
+}
