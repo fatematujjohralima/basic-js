7d32a9ac24c6df2305f581a416dd57eff1c7467c
fatematujjohra <fatematujjohralima2@gmail.com>
2026-02-07 09:53:02 +0600

    trim()

--- compare.js
+++ compare.js
@@ -7,3 +7,14 @@ if(subject.toLowerCase() === book.toLowerCase()){
 else {
     console.log('other subject');
 }
+
+// trim() string er age o porer space sorai fele....kintu vitorer space sorai na
+const drink = ' water';
+const liquid = 'wAter '
+
+if(drink.trim() === liquid.trim()){
+    console.log('water');
+}
+else {
+    console.log('jibon na')
+}
